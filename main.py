be381f78e0c1bf88b714d00961f7cb049fbeb6f6
Artem Penevskoi <apenyevskoi@gmail.com>
2023-01-17 18:32:50 +0600

    Tasks from ProjectEuler.net

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-def newfunc():
-    print()
+class textoutput:
+    BOLD_UNDERLINE = '\033[1m' + '\033[4m'
+    END = '\033[0m'
+
+def problem1():
+    return print('problem 1: sum of numbers below 1000 are multiples by 3 and 5 is',
+                 textoutput.BOLD_UNDERLINE + str(sum([ num for num in range(1,1000,1) if (num % 3 == 0) or (num % 5 == 0) ])))
 
 
 if __name__ == '__main__':
-    print()
+    problem1()
